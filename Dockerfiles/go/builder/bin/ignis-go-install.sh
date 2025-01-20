919aecc957e9cc6312e33837ddd1ef2336314666
César Pomar <cesarpomar18@gmail.com>
2025-01-20 20:48:53 +0100

    go PATH fix

--- Dockerfiles/go/builder/bin/ignis-go-install.sh
+++ Dockerfiles/go/builder/bin/ignis-go-install.sh
@@ -3,4 +3,6 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update
 apt -y install --no-install-recommends gcc golang-${GO_VERSION}
+ln -s /usr/lib/go-${GO_VERSION}/bin/go /usr/bin/go
+ln -s /usr/lib/go-${GO_VERSION}/bin/gofmt /usr/bin/gofmt
 rm -rf /var/lib/apt/lists/*
